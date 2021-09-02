734a8e293d0643a40fdb26fa82f7ab458e104ca2
Geraldo Moura <geraldo.moura@ymail.com>
2021-09-02 16:17:26 -0600

    First part done

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-// Your JS goes here
+for (let i = 0; i <= 80; i++) {
+    if (i % 2 === 0) {
+        const div = document.createElement('div');
+        document.body.style.width = '100vw';
+        document.body.style.height = '100vh';
+        document.body.appendChild(div);
+        div.style.width = '11.1%';
+        div.style.height = '11.1%';
+        div.style.backgroundColor = 'red';
+        div.style.float = 'left';
+    } else {
+        const div = document.createElement('div');
+        document.body.style.width = '100vw';
+        document.body.style.height = '100vh';
+        document.body.appendChild(div);
+        div.style.width = '11.1%';
+        div.style.height = '11.1%';
+        div.style.backgroundColor = 'black';
+        div.style.float = 'left';
+    }
+}
+
+
+
