5be096502c102edea15d59a3dc7caa701b29cb3d
Geraldo Moura <geraldo.moura@ymail.com>
2021-09-03 22:22:40 -0600

    adjusted body w and h

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
+document.body.style.width = '90vh';
+document.body.style.height = '90vh';
+
 for (let i = 0; i <= 80; i++) {
     const div = document.createElement('div');
     if (i % 2 === 0) {
-        document.body.style.width = '90vw';
-        document.body.style.height = '90vw';
-        document.body.style.margin = '0';
         document.body.appendChild(div);
         div.style.width = '11.1%';
         div.style.height = '11.1%';
         div.style.backgroundColor = 'red';
         div.style.float = 'left';
     } else {
-        document.body.style.width = '100vw';
-        document.body.style.height = '100vw';
         document.body.appendChild(div);
         div.style.width = '11.1%';
         div.style.height = '11.1%';
